762494c6247f09191ab0d927d1ced9ff01bc8e27
Louis Aude <louis.aude@live.fr>
2020-11-10 16:57:14 +0100

    sfassad

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_param)
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_param
+    params.require(:task).permit(:title, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
 end
